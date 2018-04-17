6a1c1359188d60a0d8d9a92b6af99b0f8ea0191c
acidjive <16022578@myrp.edu.sg>
2018-04-18 01:23:22 +0800

    FINALLY COMPLETED!

--- app/src/main/java/com/myapplicationdev/android/p01_dailygoals/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p01_dailygoals/MainActivity.java
@@ -1,7 +1,14 @@
 package com.myapplicationdev.android.p01_dailygoals;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnOk = (Button) findViewById(R.id.btnOk);
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                //Get the RadioGrp obj
+                RadioGroup rg1 = (RadioGroup)findViewById(R.id.radioGroup1);
+                RadioGroup rg2 = (RadioGroup)findViewById(R.id.radioGroup2);
+                RadioGroup rg3 = (RadioGroup)findViewById(R.id.radioGroup3);
+
+                //Get the Id of the selected radio btn in the radiogrp
+                int selectedButtonId1 = rg1.getCheckedRadioButtonId();
+                int selectedButtonId2 = rg2.getCheckedRadioButtonId();
+                int selectedButtonId3 = rg3.getCheckedRadioButtonId();
+
+                //Get the radio button object from the Id we had gotten above
+                RadioButton rb1 = (RadioButton)findViewById(selectedButtonId1);
+                RadioButton rb2 = (RadioButton)findViewById(selectedButtonId2);
+                RadioButton rb3 = (RadioButton)findViewById(selectedButtonId3);
+
+
+                //Get the editText that user keys in rj
+                EditText etRj=(EditText)findViewById(R.id.editText);
+
+                //Put the answers into an array
+                String[] info ={rb1.getText().toString(),rb2.getText().toString(),rb3.getText().toString(),etRj.getText().toString(),};
+
+                //create an intent to start another activity called
+                //DemoActivitites (which we would create later)
+                Intent i = new Intent(MainActivity.this,Summary.class);
+
+                //pass the string array holding the name and age to new activity.
+                i.putExtra("info",info);
+                startActivity(i);
+
+
+            }
+});
     }
+
 }
---
+++ app/src/main/java/com/myapplicationdev/android/p01_dailygoals/Summary.java
@@ -0,0 +1,32 @@
+package com.myapplicationdev.android.p01_dailygoals;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Summary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+
+
+        //Get the intent so as to get the "things" inside the intent
+        Intent i = getIntent();
+        //Get the String Array named "info" we passed in
+        String[] info = i.getStringArrayExtra("info");
+        //Get the text view object
+        TextView tv1 = (TextView) findViewById(R.id.textView1);
+        TextView tv2 = (TextView) findViewById(R.id.textView2);
+        TextView tv3 = (TextView) findViewById(R.id.textView3);
+        TextView tv4 = (TextView) findViewById(R.id.textView4);
+
+        //Display the name and age on the TextView
+        tv1.setText("Read up on materil before class: " + info[0] );
+        tv2.setText("Arrive on time so as not to miss important part of the lesson:  " + info[1] );
+        tv3.setText("Attempt the problem myself: " + info[2] );
+        tv4.setText("Reflection" + info[3] );
+    }
+}
